26828bdc9f7ce8e39d1e18973a1918ea5a65188b
Pavith <pavith.ganewatta@gmail.com>
2024-11-19 10:44:17 +1100

    completed base repayment calculator

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -1,6 +1,9 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MortgageCalculator {
+
+    private static final int MONTHS_IN_YEAR = 12;
     public static void main(String[] args) throws Exception {
         System.out.println("--- Mortgatge Calculator Program Start ---");
         Scanner scanner = new Scanner(System.in);
@@ -13,6 +16,15 @@ public class MortgageCalculator {
 
         System.out.println("Enter how many years your loan is");
         int termInYears = scanner.nextInt();
+
+        float monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR;
+        int numberOfPayments = termInYears * MONTHS_IN_YEAR;
+
+        double monthlyPayment = principal * ((monthlyInterestRate * 
+                                            (Math.pow(1+monthlyInterestRate,numberOfPayments)))/
+                                            ((Math.pow(1+monthlyInterestRate,numberOfPayments))-1));
+
+        System.out.println("Your Monthly Payment is: "+ NumberFormat.getCurrencyInstance().format(monthlyPayment));
 
     }
 }
